f39eb3e44b7ceda131c4c9d874b02beba52a469c
BigGafLeo <karoldzwon@gmail.com>
2022-01-06 13:31:04 +0100

    Gui kolejki gotowe do testów

--- src/main/java/com/example/trylmaproject/client/Client.java
+++ src/main/java/com/example/trylmaproject/client/Client.java
@@ -1,12 +1,16 @@
 package com.example.trylmaproject.client;
 
 import com.example.trylmaproject.server.Field;
+import com.example.trylmaproject.server.Game;
 
+import java.awt.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Client {
     String serverAddress;
@@ -20,19 +24,15 @@ public class Client {
     public Client(String serverAddress)
     {
         this.serverAddress = serverAddress;
-
-
     }
     private void run() throws IOException
     {
         try
         {
-            var socket = new Socket(serverAddress,59001);
+            var socket = new Socket(serverAddress,59090);
             in = new Scanner(socket.getInputStream());
             out = new PrintWriter(socket.getOutputStream(),true);
             ois = new ObjectInputStream(socket.getInputStream());
-
-
 
             while (in.hasNextLine())
             {
@@ -40,6 +40,8 @@ public class Client {
                 if(line.startsWith("NUMER: "))
                     playerNumber = Integer.parseInt(line.substring(8));
                 queFrame = new QueFrame(playerNumber);
+                queFrameCreaction();
+
                 if (line.startsWith("IMIE:") && queFrame.isNameReadyToSend())
                     out.println(queFrame.getNameToServer());
                 if(playerNumber == 1){
@@ -52,5 +54,17 @@ public class Client {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+    private void queFrameCreaction()
+    {
+        queFrame.setDefaultCloseOperation(3);
+        queFrame.setTitle("Test");
+        queFrame.setVisible(true);
+        queFrame.setLocationRelativeTo((Component) null);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client("localhost");
+
     }
 }
