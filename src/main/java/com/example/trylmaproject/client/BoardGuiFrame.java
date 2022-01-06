f2b34632c0deb6a5a9e9bff3b97d1549eab9ccb1
BigGafLeo <karoldzwon@gmail.com>
2022-01-06 20:47:31 +0100

    Gui Planszy

--- src/main/java/com/example/trylmaproject/client/BoardGuiFrame.java
+++ src/main/java/com/example/trylmaproject/client/BoardGuiFrame.java
@@ -1,19 +1,59 @@
 package com.example.trylmaproject.client;
 
+import com.example.trylmaproject.server.Board;
+import com.example.trylmaproject.server.Field;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class BoardGuiFrame extends JFrame
 {
-	private JPanel boardPanel;
-	private static final int DEFAULT_FIELD_LENGHT = 40;
-	private final static int DEFAULT_HEIGHT = 17*DEFAULT_FIELD_LENGHT;
-	private final static int DEFAULT_WIDTH = 25*DEFAULT_FIELD_LENGHT;
+	private JPanel panel;
+	private int playerNumber;
 
-	public BoardGuiFrame(int playerCount)
+	public BoardGuiFrame(int playerNumber, Board board)
 	{
-		this.boardPanel = new BoardGuiPanel(playerCount);
-		this.getContentPane().add(this.boardPanel);
-		this.setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
-	}
+		panel = new BoardGuiPanel(board);
+		this.playerNumber = playerNumber;
 
+	}
 }
+//public class BoardGuiFrame extends JFrame
+//{
+//	private JPanel boardPanel;
+////	private static final int DEFAULT_FIELD_LENGHT = 40;
+////	private final static int DEFAULT_HEIGHT = 17*DEFAULT_FIELD_LENGHT;
+////	private final static int DEFAULT_WIDTH = 25*DEFAULT_FIELD_LENGHT;
+//
+//	public BoardGuiFrame(int playerCount)
+//	{
+//		this.boardPanel = new BoardGuiPanel();
+//		this.getContentPane().add(this.boardPanel);
+////		this.setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+////		add(new BoardGuiPanel());
+////		add(new DrawComponent());
+//		this.setSize(BoardGuiPanel.DEFAULT_BOARD_WIDTH, BoardGuiPanel.DEFAULT_BOARD_HEIGHT);
+//	}
+//
+//
+//}
+//class DrawComponent extends JComponent
+//{
+//	public final static int DEFAULT_PAWN_RADIUS = 15;
+//	public final static int DEFAULT_BOARD_WIDTH = 13 * DEFAULT_PAWN_RADIUS * 2 + 12 * DEFAULT_PAWN_RADIUS;
+//	public final static int DEFAULT_BOARD_HEIGHT = 17 * DEFAULT_PAWN_RADIUS * 2 + 16 * DEFAULT_PAWN_RADIUS;
+//	public void paintComponent(Graphics g)
+//	{
+//		Graphics2D g2 = (Graphics2D) g;
+//
+//		int centerX = 19 * DEFAULT_PAWN_RADIUS;
+//		int centerY = 1 * DEFAULT_PAWN_RADIUS;
+//
+//		Ellipse2D circle = new Ellipse2D.Double();
+//		circle.setFrameFromCenter(centerX,centerY,centerX + DEFAULT_PAWN_RADIUS,centerY + DEFAULT_PAWN_RADIUS);
+//		g2.draw(circle);
+//	}
+//}
+
--- src/main/java/com/example/trylmaproject/client/test/BordGuiTest.java
+++ src/main/java/com/example/trylmaproject/client/test/BordGuiTest.java
@@ -1,6 +1,9 @@
 package com.example.trylmaproject.client.test;
 
 import com.example.trylmaproject.client.BoardGuiFrame;
+import com.example.trylmaproject.exceptions.IllegalNumberOfPlayers;
+import com.example.trylmaproject.server.Board;
+import com.example.trylmaproject.server.Field;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,7 +11,13 @@ import java.awt.*;
 public class BordGuiTest {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(()->{
-			JFrame frame = new BoardGuiFrame(2);
+			Board board = null;
+			try {
+				board = new Board(2);
+			} catch (IllegalNumberOfPlayers e) {
+				e.printStackTrace();
+			}
+			JFrame frame = new BoardGuiFrame(1,board);
 			frame.setDefaultCloseOperation(3);
 			frame.setTitle("Test");
 			frame.setVisible(true);
