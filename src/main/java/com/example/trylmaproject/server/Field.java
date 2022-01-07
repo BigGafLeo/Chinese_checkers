2d95f35f5065e32ca6bf447a02443de147c9be91
BigGafLeo <karoldzwon@gmail.com>
2022-01-07 10:30:42 +0100

    Update field

--- src/main/java/com/example/trylmaproject/server/Field.java
+++ src/main/java/com/example/trylmaproject/server/Field.java
@@ -1,6 +1,7 @@
 package com.example.trylmaproject.server;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 import java.io.Serializable;
 
@@ -14,7 +15,9 @@ public class Field implements Serializable {
     private int posx;
     private int posy;
     private int playerNumber;
-    //private Color color;
+    private Circle circle;
+    private Color color;
+    private static final int DEFAULT_RADIUS = 15;
     //private Color[] colorList = {Color.BLACK, Color.GREEN, Color.BLUE, Color.YELLOW, Color.RED, Color.PURPLE, Color.BROWN};
 
     public int getPlayerNumber() {
@@ -46,5 +49,11 @@ public class Field implements Serializable {
     Field(int posy, int posx){
         this.posx=posx;
         this.posy=posy;
+        circle = new Circle(posx,posy,DEFAULT_RADIUS);
+    }
+
+    public boolean isHit(int x, int y)
+    {
+        return circle.getBoundsInLocal().contains(x,y);
     }
 }
