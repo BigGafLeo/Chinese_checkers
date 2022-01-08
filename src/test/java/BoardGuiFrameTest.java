c8325924b33dd1179b02b323dce20401d27225d8
mteplicki <261726@student.pwr.edu.pl>
2022-01-08 14:28:53 +0100

    Dodano klasę testującą działanie GUI: do parametrów uruchamiania KONIECZNIE trzeba dodać -ea --add-exports com.example.trylmaproject/com.example.trylmaproject.client=ALL-UNNAMED

---
+++ src/test/java/BoardGuiFrameTest.java
@@ -0,0 +1,30 @@
+import com.example.trylmaproject.client.BoardGuiFrame;
+import com.example.trylmaproject.exceptions.IllegalNumberOfPlayers;
+import com.example.trylmaproject.server.Board;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+public class BoardGuiFrameTest {
+    @Test
+    public void testGUI(){
+        Board board = null;
+        try {
+            board = new Board(2);
+        } catch (IllegalNumberOfPlayers e) {
+            e.printStackTrace();
+        }
+        BoardGuiFrame boardGuiFrame = new BoardGuiFrame(2, board.getBoard());
+        boardGuiFrame.setDefaultCloseOperation(3);
+        boardGuiFrame.setTitle("Warcaby");
+        boardGuiFrame.setVisible(true);
+        boardGuiFrame.setLocationRelativeTo((Component) null);
+        synchronized (this){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
